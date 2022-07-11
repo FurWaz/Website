b127718e69c847c2d810716071f2a032b8c18d1d
FurWaz <fur.waz06@gmail.com>
2022-07-11 21:30:55 +0200

    Created default server to test deployment

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { Sequelize } from "sequelize";
+
+const app = express();
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mariadb'
+});
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err.toString());
+});
