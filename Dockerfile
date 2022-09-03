c48cad35905c6fe9ee22eceb81179d1885ed6239
FurWaz <fur.waz06@gmail.com>
2022-09-03 20:02:22 +0200

    Started new UI

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-# étape de build
-#FROM node:lts-alpine as build-stage
 FROM node:lts-alpine
 RUN npm install -g http-server
 WORKDIR /app
@@ -9,10 +7,3 @@ COPY . .
 RUN npm run build
 EXPOSE 8080
 CMD [ "http-server", "-P", "http://localhost:8080?", "dist" ]
-
-# étape de production
-#FROM nginx:stable-alpine as production-stage
-#COPY --from=build-stage /app/dist /usr/share/nginx/html
-#COPY ./nginx.conf /etc/nginx/conf.d/default.conf
-#EXPOSE 80
-#CMD ["nginx", "-g", "daemon off;"]
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'white': '#ecf0f1',
-      'white-dark': '#bdc3c7',
-      'black': '#34495e',
-      'black-dark': '#2c3e50',
-      'gray': '#95a5a6',
-      'gray-dark': '#7f8c8d',
-      'blue': '#3498db',
-      'blue-dark': '#2980b9',
-      'green': '#2ecc71',
-      'green-dark': '#27ae60',
-      'red': '#e74c3c',
-      'red-dark': '#c0392b',
-      'yellow': '#f1c40f',
-      'yellow-dark': '#f39c12',
-      'orange': '#e67e22',
-      'orange-dark': '#d35400',
-      'purple': '#9b59b6',
-      'purple-dark': '#8e44ad',
-      'turquoise': '#1abc9c',
-      'turquoise-dark': '#16a085',
-    },
+    extend: {
+      colors: {
+        primary: {
+          50: "#ECFDF5",
+          100: "#D1FAE5",
+          200: "#A7F3D0",
+          300: "#6EE7B7",
+          400: "#34D399",
+          500: "#10B981",
+          600: "#059669",
+          700: "#047857",
+          800: "#065F46",
+        },
+      }
+    }
   },
   plugins: [
     
