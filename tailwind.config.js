a2ab55a6c4ff3a0ac94d03aefa46b098b5b2c2b7
FurWaz <fur.waz06@gmail.com>
2022-09-13 17:11:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,7 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: "#ECFDF5",
-          100: "#D1FAE5",
-          200: "#A7F3D0",
-          300: "#6EE7B7",
-          400: "#34D399",
-          500: "#10B981",
-          600: "#059669",
-          700: "#047857",
-          800: "#065F46",
-        },
-      }
-    }
+    extend: {}
   },
   plugins: [
     
