5b7e8f20f53c4f8abf8caaecd42c46b6aaad1ef4
FurWaz <fur.waz06@gmail.com>
2022-09-13 17:11:38 +0200

    Moved topbar to own component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
     <div class="w-screen h-screen bg-slate-700">
-        <div class="absolute w-screen h-fit">
-            <div class="flex grow h-fit m-4 rounded-lg p-3 bg-primary-600 shadow-xl">
-                <div class="flex flex-col justify-center">
-                    <h1 class="text-white text-2xl font-bold m-0">FurWaz</h1>
-                </div>
-            </div>
-        </div>
+        <topbar></topbar>
     </div>
 </template>
 
 <script>
+import Topbar from '../components/Topbar.vue'
 
 export default {
     name: "Home",
-    components: {},
+    components: {
+        Topbar
+    },
     methods: {},
     data() { return {}; },
     setup() {},
