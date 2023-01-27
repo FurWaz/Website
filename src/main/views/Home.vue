09672f03f88095e2966456c57750749bbd973ed8
FurWaz <fur.waz06@gmail.com>
2023-01-27 16:01:50 +0100

    Delayed animation to avoid lag

--- src/main/views/Home.vue
+++ src/main/views/Home.vue
@@ -1,23 +1,24 @@
 <template>
     <div class="relative flex grow overflow-x-hidden">
         <div class="absolute top-0 left-0 w-0 h-0">
-            <div ref="particles" class="relative flex justify-center h-[80vh] w-screen">
+            <div ref="particles" class="relative flex justify-center h-[90vh] w-screen">
 
             </div>
         </div>
         <div class="flex flex-col grow" style="z-index: 1;">
             <div class="flex grow justify-center my-40">
                 <div class="flex flex-col justify-center">
-                    <div class="show-up duration-1000 flex bg-slate-800 md:border-4 border-2 border-slate-600 md:rounded-3xl rounded-xl md:px-20 px-6 h-fit shadow-xl">
-                        <h1 class="md:text-[8em] text-[3em] font-extrabold drop-shadow-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-orange-500 m-0"> FurWaz </h1>
+                    <div class="show-up flex bg-slate-800 md:border-4 border-2 border-slate-600 md:rounded-3xl rounded-xl md:px-20 px-6 h-fit shadow-xl">
+                        <h1 style="animation-delay: 200ms"
+                            class="show-up md:text-[8em] text-[3em] font-extrabold drop-shadow-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-orange-500 m-0"> FurWaz </h1>
                     </div>
                 </div>
             </div>
             <div class="flex grow-0 h-fit">
                 <img src="../../assets/UI/waves-orange.svg" alt="" class="w-full">
-            </div>
-            <div class="flex grow bg-orange-500">
+                <div class="flex grow bg-orange-500">
 
+                </div>
             </div>
         </div>
     </div>
@@ -31,7 +32,7 @@ function spawnSVG(dom, size, pos, rot, color) {
     `;
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     const div = document.createElement("div");
-    const randrot = Math.random() * 20;
+    const randrot = Math.random() * 200 - 100;
 
     svg.setAttribute("viewBox", "-155 -160 310 310");
     div.style.position = "absolute";
@@ -59,13 +60,15 @@ function spawnParticles(obj) {
     const div = obj.$refs["particles"];
     const colors = ["4e3967", "1e293b", "2a304d", "543a69", "6f3c72", "8c3c75", "c23b6a", "a83a72", "d8405d", "ea4b4c", "f55d37", "f97316"];
 
-    for(let i = 0; i < 20; i++) {
-        const size = Math.random() * 5 + 3;
-        const pos = {x: Math.random() * 100, y: Math.random() * 90};
-        const rot = Math.random() * 360;
-        const colorIndex = (size-3) * colors.length / 5;
-        const color = "#" + colors[Math.floor(colorIndex)];
-        spawnSVG(div, size, pos, rot, color);
+    for (let i = 0; i < 20; i++) {
+        setTimeout(() => {
+            const size = Math.random() * 5 + 3;
+            const pos = {x: Math.random() * 100, y: Math.random() * 90};
+            const rot = Math.random() * 360;
+            const colorIndex = (size-3) * colors.length / 5;
+            const color = "#" + colors[Math.floor(colorIndex)];
+            spawnSVG(div, size, pos, rot, color);
+        }, 100 * i);
     }
 }
 
