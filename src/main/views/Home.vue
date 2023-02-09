ca6970bb82ce45e629767af06a5342f621c44fa2
FurWaz <fur.waz06@gmail.com>
2023-02-09 16:47:13 +0100

    Fixed mobile view

--- src/main/views/Home.vue
+++ src/main/views/Home.vue
@@ -5,7 +5,7 @@
 
             </div>
         </div>
-        <div class="flex flex-col grow" style="z-index: 1;">
+        <div class="flex flex-col grow w-full" style="z-index: 1;">
             <div class="flex grow justify-center my-40">
                 <div ref="title" class="flex flex-col justify-center">
                     <div class="show-down noscroll flex dark:bg-slate-800 bg-slate-300 md:border-4 border-2 dark:border-slate-600 border-slate-400 md:rounded-3xl rounded-xl md:px-20 px-6 h-fit shadow-xl">
@@ -15,20 +15,21 @@
                 </div>
             </div>
             <div class="show-up flex flex-col grow-0 h-fit">
+                
                 <img v-if="isDarkMode" src="../../assets/UI/waves-dark.svg" alt="" class="w-full scale-[1.01]">
                 <img v-if="!isDarkMode" src="../../assets/UI/waves.svg" alt="" class="w-full scale-[1.01]">
-                <div class="flex grow bg-orange-500 h-[60vh] w-full">
-                    <div class="flex flex-col justify-center grow w-[50%] px-10">
-                        <h1 class="show-up text-6xl text-slate-700 font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.Applications }} </h1>
-                        <p class="show-up text-2xl text-slate-700 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ApplicationsDesc1 }} </p>
-                        <p class="show-up text-2xl text-slate-700 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ApplicationsDesc2 }} </p>
+                <div class="flex md:flex-row flex-col grow bg-orange-500 min-h-[60vh] h-fit w-full py-8 md:space-y-0 space-y-8">
+                    <div class="flex flex-col justify-center grow md:w-[50%] md:px-10 px-4">
+                        <h1 class="show-up md:text-6xl text-4xl text-slate-700 font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.Applications }} </h1>
+                        <p class="show-up md:text-2xl text-xl text-slate-700 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ApplicationsDesc1 }} </p>
+                        <p class="show-up md:text-2xl text-xl text-slate-700 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ApplicationsDesc2 }} </p>
                     </div>
-                    <div class="flex flex-col justify-evenly grow w-[50%] px-10">
+                    <div class="flex flex-col justify-evenly grow md:w-[50%] md:px-10 px-4">
                         <div class="flex flex-wrap justify-center space-x-8">
                             <div v-for="el in appsIcons" :key="el" class="show-down" :style="'animation-delay: '+(appsIcons.indexOf(el) * 100 + 100)+'ms'">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
                                     stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
-                                    v-html="el.icon" class="text-slate-700 w-24" :style="'transform: rotate('+el.rot+'deg);'">
+                                    v-html="el.icon" class="text-slate-700 md:w-24 w-16" :style="'transform: rotate('+el.rot+'deg);'">
                                 </svg>
                             </div>
                         </div>
@@ -39,13 +40,19 @@
                 </div>
                 <img v-if="isDarkMode" src="../../assets/UI/waves-dark.svg" alt="" class="w-full -scale-[1.01]">
                 <img v-if="!isDarkMode" src="../../assets/UI/waves.svg" alt="" class="w-full -scale-[1.01]">
-                <div class="flex grow bg-transparent h-[60vh] w-full">
-                    <div class="flex flex-col justify-evenly grow w-[50%] px-10">
+
+                <div class="flex md:flex-row flex-col grow bg-transparent h-[60vh] w-full">
+                    <div class="md:hidden flex flex-col justify-center grow md:w-[50%] md:px-10 px-4">
+                        <h1 class="show-up md:text-6xl text-4xl text-slate-400 font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.Projects }} </h1>
+                        <p class="show-up md:text-2xl text-xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ProjectsDesc1 }} </p>
+                        <p class="show-up md:text-2xl text-xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ProjectsDesc2 }} </p>
+                    </div>
+                    <div class="flex flex-col justify-evenly grow md:w-[50%] md:px-10 px-4">
                         <div class="flex flex-wrap justify-center space-x-8">
                             <div v-for="el in projectsIcons" :key="el" class="show-down" :style="'animation-delay: '+(appsIcons.indexOf(el) * 100 + 100)+'ms'">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
                                     stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
-                                    v-html="el.icon" class="text-slate-400 w-24" :style="'transform: rotate('+el.rot+'deg);'">
+                                    v-html="el.icon" class="text-slate-400 md:w-24 w-16" :style="'transform: rotate('+el.rot+'deg);'">
                                 </svg>
                             </div>
                         </div>
@@ -53,32 +60,38 @@
                             <button-block href="/projects"> {{ Lang.CurrentLang.SeeProjects }} </button-block>
                         </div>
                     </div>
-                    <div class="flex flex-col justify-center grow w-[50%] px-10">
-                        <h1 class="show-up text-6xl text-slate-400 font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.Projects }} </h1>
-                        <p class="show-up text-2xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ProjectsDesc1 }} </p>
-                        <p class="show-up text-2xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ProjectsDesc2 }} </p>
+                    <div class="md:flex hidden flex-col justify-center grow md:w-[50%] md:px-10 px-4">
+                        <h1 class="show-up md:text-6xl text-4xl text-slate-400 font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.Projects }} </h1>
+                        <p class="show-up md:text-2xl text-xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ProjectsDesc1 }} </p>
+                        <p class="show-up md:text-2xl text-xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.ProjectsDesc2 }} </p>
                     </div>
                 </div>
-                <div class="flex grow h-fit w-full">
-                    <div class="flex flex-col grow w-[50%]">
-                        <div v-if="!isDarkMode" class="show-right mr-auto">
-                            <img src="../../assets/UI/blob.svg" alt="" class="h-80">
-                            <img src="../../assets/UI/blob.svg" alt="" class="h-80 rotate-90">
-                        </div>
-                        <div v-if="isDarkMode" class="show-right mr-auto">
-                            <img src="../../assets/UI/blob-dark.svg" alt="" class="h-80">
-                            <img src="../../assets/UI/blob-dark.svg" alt="" class="h-80 rotate-90">
-                        </div>
+
+                <div class="flex md:flex-row flex-col grow h-fit w-full">
+                    <div class="md:hidden flex flex-col grow">
+                        <img src="../../assets/UI/wave.svg" alt="" class="w-full">
                     </div>
-                    <div class="flex flex-col justify-center grow w-[50%] px-10">
-                        <h1 class="show-up text-6xl text-slate-400 font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.About }} </h1>
-                        <p class="show-up text-2xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.AboutDesc1 }} </p>
-                        <p class="show-up text-2xl text-slate-400 font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.AboutDesc2 }} </p>
+                    <div class="flex flex-col justify-center grow md:w-[50%] md:px-10 px-4 pb-10 pt-14 md:bg-transparent bg-orange-500 md:text-slate-400 text-slate-700">
+                        <h1 class="show-up md:text-6xl text-4xl font-bold text-center mx-auto mb-8"> {{ Lang.CurrentLang.About }} </h1>
+                        <p class="show-up md:text-2xl text-xl font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.AboutDesc1 }} </p>
+                        <p class="show-up md:text-2xl text-xl font-bold text-center mx-auto" style="animation-delay: 200ms"> {{ Lang.CurrentLang.AboutDesc2 }} </p>
                         <div class="show-up flex justify-center" style="animation-delay: 500ms">
-                            <button-block href="/about" class="w-fit mx-auto mt-8"> {{ Lang.CurrentLang.SeeAbout }} </button-block>
+                            <button-block href="/about" class="md:flex hidden w-fit mx-auto mt-8"> {{ Lang.CurrentLang.SeeAbout }} </button-block>
+                            <button-block href="/about" color="black" class="md:hidden flex w-fit mx-auto mt-8"> {{ Lang.CurrentLang.SeeAbout }} </button-block>
+                        </div>
+                    </div>
+                    <div class="md:flex hidden flex-col grow w-[50%]">
+                        <div v-if="!isDarkMode" class="show-right ml-auto">
+                            <img src="../../assets/UI/blob.svg" alt="" class="h-80 -rotate-90">
+                            <img src="../../assets/UI/blob.svg" alt="" class="h-80 rotate-180">
+                        </div>
+                        <div v-if="isDarkMode" class="show-right ml-auto">
+                            <img src="../../assets/UI/blob-dark.svg" alt="" class="h-80 -rotate-90">
+                            <img src="../../assets/UI/blob-dark.svg" alt="" class="h-80 rotate-180">
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
@@ -151,18 +164,6 @@ function setupAnimation(page) {
 const appsIcons = [
     {rot: Math.random() * 40 - 20, icon: `<path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />`},
     {rot: Math.random() * 40 - 20, icon: `<path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />`},
-    {rot: Math.random() * 40 - 20, icon: `<path d="M2.97 12.92A2 2 0 0 0 2 14.63v3.24a2 2 0 0 0 .97 1.71l3 1.8a2 2 0 0 0 2.06 0L12 19v-5.5l-5-3-4.03 2.42Z"></path>
-        <path d="m7 16.5-4.74-2.85"></path>
-        <path d="m7 16.5 5-3"></path>
-        <path d="M7 16.5v5.17"></path>
-        <path d="M12 13.5V19l3.97 2.38a2 2 0 0 0 2.06 0l3-1.8a2 2 0 0 0 .97-1.71v-3.24a2 2 0 0 0-.97-1.71L17 10.5l-5 3Z"></path>
-        <path d="m17 16.5-5-3"></path>
-        <path d="m17 16.5 4.74-2.85"></path>
-        <path d="M17 16.5v5.17"></path>
-        <path d="M7.97 4.42A2 2 0 0 0 7 6.13v4.37l5 3 5-3V6.13a2 2 0 0 0-.97-1.71l-3-1.8a2 2 0 0 0-2.06 0l-3 1.8Z"></path>
-        <path d="M12 8 7.26 5.15"></path>
-        <path d="m12 8 4.74-2.85"></path>
-        <path d="M12 13.5V8"></path>`},
     {rot: Math.random() * 40 - 20, icon: `<line x1="6" y1="12" x2="10" y2="12"></line>
         <line x1="8" y1="10" x2="8" y2="14"></line>
         <line x1="15" y1="13" x2="15.01" y2="13"></line>
