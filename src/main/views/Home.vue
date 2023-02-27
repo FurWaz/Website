33ec233978f625168920330b08c7b8e9428262ba
FurWaz <fur.waz06@gmail.com>
2023-02-27 08:23:27 +0100

    Changed icon and home page

--- src/main/views/Home.vue
+++ src/main/views/Home.vue
@@ -6,12 +6,29 @@
             </div>
         </div>
         <div class="flex flex-col grow w-full" style="z-index: 1;">
-            <div class="flex grow justify-center my-40">
-                <div ref="title" class="flex flex-col justify-center">
-                    <div class="show-down noscroll flex dark:bg-slate-800 bg-slate-300 md:border-4 border-2 dark:border-slate-600 border-slate-400 md:rounded-3xl rounded-xl md:px-20 px-6 h-fit shadow-xl">
-                        <h1 style="animation-delay: 200ms"
-                            class="show-down noscroll md:text-[8em] text-[3em] font-extrabold drop-shadow-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-orange-500 m-0"> FurWaz </h1>
-                    </div>
+            <div ref="title"  class="flex flex-col grow justify-center my-20">
+                <div style="animation-delay: 300ms" class="show-up noscroll opacity-0 flex justify-center mx-auto transition-all dark:text-slate-50 text-slate-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 250 250" stroke-width="14" stroke="currentColor" class="md:h-80 h-40 drop-shadow-xl">
+                        <path d="M-45,34"/>
+                        <path d="M144,52"/>
+                        <path d="M198,196"/>
+                        <path d="M36,70"/>
+                        <path d="M135,178"/>
+                        <path d="M270,106"/>
+                        <g>
+                            <polygon points="54,142 98.9,178.1 116.9,196 135,196 153,178.1 198,142.1 189,124 198,70 189,52 153,88 99,88 63,52 
+                                54,70 63,124 	"/>
+                            <g>
+                                <path fill="currentColor" d="M54,142.1l44.9,9c0,0,0,0,0,0l18,44.9c0,0,0,0,0,0l-17.9-17.9c0,0,0,0,0,0L54,142.1
+                                    C54,142.1,54,142.1,54,142.1z"/>
+                                <path fill="currentColor" d="M198,142.1l-45,9c0,0,0,0,0,0L135,196c0,0,0,0.1,0,0l17.9-17.9c0,0,0,0,0,0L198,142.1
+                                    C198,142.1,198,142.1,198,142.1z"/>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
+                <div class="flex flex-col justify-center mx-auto">
+                    <h1 style="animation-delay: 200ms" class="show-down noscroll md:text-[8em] text-[4em] font-extrabold drop-shadow-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-orange-500 m-0"> FurWaz </h1>
                 </div>
             </div>
             <div class="show-up flex flex-col grow-0 h-fit">
