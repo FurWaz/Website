59d37682726341126b3c31c001cee1ca7d849ebb
FurWaz <fur.waz06@gmail.com>
2025-08-07 08:36:53 +0200

    Fixed project card style and auto popup, added project select scroll

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,60 +1,62 @@
 <template>
-    <UModal v-model="modalOpen">
-        <button class="flex flex-col space-y-4 card p-4 w-80 h-full bg-white dark:bg-slate-700 hover:border-slate-300 hover:dark:border-slate-500 transition-all" @click="openModal">
-            <div class="flex h-full justify-start items-center space-x-4">
-                <img :src="project.image" class="w-12 h-12 rounded-md bg-slate-200 dark:bg-slate-800" :class="project.imagePadding? 'p-1.5' : ''"/>
-                <h2> {{ text(project.name) }} </h2>
+    <button ref="btn" class="flex flex-col space-y-4 m-8 p-4 w-80 h-full cursor-pointer bg-white dark:bg-slate-700 rounded-md shadow-md border hover:border-slate-300 hover:dark:border-slate-500 transition-all" @click="openModal"
+        :class="modalOpen? 'border-primary dark:border-primary' : 'border-slate-200 dark:border-slate-600'">
+        <div class="flex h-full justify-start items-center space-x-4">
+            <img :src="project.image" class="w-12 h-12 rounded-md bg-slate-200 dark:bg-slate-800" :class="project.imagePadding? 'p-1.5' : ''"/>
+            <h2> {{ text(project.name) }} </h2>
+        </div>
+        <div class="flex flex-col grow w-full h-full py-2">
+            <p class="text-slate-600 dark:text-slate-300"> {{ text(project.description) }} </p>
+        </div>
+        <div class="flex overflow-auto w-full h-fit pb-3">
+            <div class="flex space-x-4 w-fit h-fit">
+                <div v-for="tag in project.tags" :key="tag" class="flex rounded bg-slate-200 dark:bg-slate-800 px-1">
+                    <p class="whitespace-nowrap"> #{{ tag }} </p>
+                </div>
             </div>
-            <div class="flex flex-col grow w-full h-full py-2">
-                <p class="text-slate-600 dark:text-slate-300"> {{ text(project.description) }} </p>
-            </div>
-            <div class="flex overflow-auto w-full h-fit pb-3">
-                <div class="flex space-x-4 w-fit h-fit">
-                    <div v-for="tag in project.tags" :key="tag" class="flex rounded bg-slate-200 dark:bg-slate-800 px-1">
-                        <p class="whitespace-nowrap"> #{{ tag }} </p>
+        </div>
+        <div class="flex min-w-fit h-full justify-between items-center">
+            <UButton v-if="project.links.website" :to="project.links.website" trailing variant="solid" target="_blank">
+                {{ $t('projects.action.seeWebsite') }}
+            </UButton>
+            <UButton v-if="project.links.github" :to="project.links.github" variant="ghost" target="_blank">
+                {{ $t('projects.action.seeCode') }}
+            </UButton>
+        </div>
+        <UModal v-model:open="modalOpen">
+            <template #content>
+                <div class="p-4">
+                    <div class="flex justify-center items-center space-x-4">
+                        <img :src="project.image" class="w-16 h-16 rounded-md bg-slate-200 dark:bg-slate-800" :class="project.imagePadding? 'p-1.5' : ''"/>
+                        <div>
+                            <h2> {{ text(project.name) }} </h2>
+                            <p class="italic text-slate-600 dark:text-slate-300"> {{ text(project.description) }} </p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col space-y-4 text-center py-8">
+                        <p v-for="sentence in text(project.explanation)">
+                            {{ sentence }}
+                        </p>
+                    </div>
+                    <div class="flex min-w-fit h-full justify-between items-center">
+                        <UButton v-if="project.links.website" :to="project.links.website" trailing variant="solid" target="_blank">
+                            {{ $t('projects.action.seeWebsite') }}
+                        </UButton>
+                        <UButton v-if="project.links.github" :to="project.links.github" variant="ghost" target="_blank">
+                            {{ $t('projects.action.seeCode') }}
+                        </UButton>
                     </div>
                 </div>
-            </div>
-            <div class="flex min-w-fit h-full justify-between items-center">
-                <UButton v-if="project.links.website" :to="project.links.website" trailing variant="solid" target="_blank">
-                    {{ $t('projects.action.seeWebsite') }}
-                </UButton>
-                <UButton v-if="project.links.github" :to="project.links.github" variant="ghost" target="_blank">
-                    {{ $t('projects.action.seeCode') }}
-                </UButton>
-            </div>
-        </button>
-        <template #content>
-            <div class="p-4">
-                <div class="flex justify-center items-center space-x-4">
-                    <img :src="project.image" class="w-16 h-16 rounded-md bg-slate-200 dark:bg-slate-800" :class="project.imagePadding? 'p-1.5' : ''"/>
-                    <div>
-                        <h2> {{ text(project.name) }} </h2>
-                        <p class="italic text-slate-600 dark:text-slate-300"> {{ text(project.description) }} </p>
-                    </div>
-                </div>
-                <div class="flex flex-col space-y-4 text-center py-8">
-                    <p v-for="sentence in text(project.explanation)">
-                        {{ sentence }}
-                    </p>
-                </div>
-                <div class="flex min-w-fit h-full justify-between items-center">
-                    <UButton v-if="project.links.website" :to="project.links.website" trailing variant="solid" target="_blank">
-                        {{ $t('projects.action.seeWebsite') }}
-                    </UButton>
-                    <UButton v-if="project.links.github" :to="project.links.github" variant="ghost" target="_blank">
-                        {{ $t('projects.action.seeCode') }}
-                    </UButton>
-                </div>
-            </div>
-        </template>
-    </UModal>
+            </template>
+        </UModal>
+    </button>
 </template>
 
 <script lang="ts" setup>
 import type { Project } from '~/composables/Projects';
 
 const router = useRouter();
+const btn = ref<HTMLButtonElement>();
 
 const props = defineProps<{
     project: Project;
@@ -66,11 +68,26 @@ watch(() => router.currentRoute.value.query.project, (project) => {
 });
 
 onMounted(() => {
-    if (router.currentRoute.value.query.project === props.project.name)
+    if (router.currentRoute.value.query.project === props.project.name) {
         modalOpen.value = true;
+    }
 });
 
 const modalOpen = ref(false);
+watch(modalOpen, (val, old) => {
+    if (modalOpen.value) {
+        setTimeout(() => {
+            if (!btn.value) return;
+            const rect = btn.value.getBoundingClientRect();
+            window.scrollBy({top: rect.top - window.innerHeight/2+rect.height/2, behavior: 'smooth'});
+        }, 10);
+    }
+
+    if (!modalOpen.value && router.currentRoute.value.query.project === props.project.name) {
+        router.push({ query: { project: undefined } });
+    }
+});
+
 function openModal() {
     modalOpen.value = true;
     router.push({ query: { project: props.project.name } });
